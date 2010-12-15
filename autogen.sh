9f72ed7975ee27bf03d9e25247f4ce437d62b538
W. Michael Petullo <mike@flyn.org>
2010-12-15 17:03:52 -0600

    Build changes to ensure Vala is optional Signed-off-by: W. Michael Petullo <mike@flyn.org>

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,6 @@ autoconf || exit 1
 autoheader || exit 1
 libtoolize --force || glibtoolize --force || exit 1
 automake -a || exit 1
-./configure $* || exit 1
+./configure --enable-maintainer-mode $* || exit 1
 
 exit 0
