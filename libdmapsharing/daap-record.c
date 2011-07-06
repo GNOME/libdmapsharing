f4aef794f5500e8a545252e7af4780b18c24675f
W. Michael Petullo <mike@flyn.org>
2011-07-06 17:42:24 -0500

    Remove fixed FIXME comment Signed-off-by: W. Michael Petullo <mike@flyn.org>

--- libdmapsharing/daap-record.c
+++ libdmapsharing/daap-record.c
@@ -45,7 +45,6 @@ daap_record_init (DAAPRecordIface * iface)
 						      "Song title", "Unknown",
 						      G_PARAM_READWRITE));
 
-		/* FIXME: This is actually an enum */
 		g_object_interface_install_property (iface,
 						     g_param_spec_enum
 						     ("mediakind",
